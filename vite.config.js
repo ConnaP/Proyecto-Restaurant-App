886b0ec82071b6b5775e55e67419f768311b7ee5
coni <coni.palavecino@gmail.com>
2023-07-08 23:42:10 -0400

    fix

--- vite.config.js
+++ vite.config.js
@@ -3,11 +3,11 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
-export default defineConfig(({ command }) => {
-  const config = {
-    plugins: [react()],
-    base: 'https://github.com/ConnaP.github.io/Proyecto-Restaurant-App/',
-  }
+export default defineConfig({
+  // const config = {
+  plugins: [react()],
+  base: 'https://github.com/ConnaP.github.io/Proyecto-Restaurant-App/',
+  // }
 
   // if (command !== 'serve') {
   //   config.base = '/react-vite-gh-pages/'
